f367e66c1649f5d4092f44bce979e986706ecf9d
Navin Tiwari <navintiwari08@yahoo.com>
2024-01-31 00:39:21 +0530

    Modify UNet. Add activation enable/disable support. Remove Batch norms.

--- medical-segmentation/models.py
+++ medical-segmentation/models.py
@@ -9,17 +9,25 @@ __all__ = ['UNet']
 class DoubleConv(nn.Module):
     """(conv => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self, in_channels, out_channels, mid_channels=None, activations=True):
         super(DoubleConv, self).__init__()
         if not mid_channels:
             mid_channels = out_channels
-        self.double_conv = nn.Sequential(
+        if activations:
+            self.double_conv = nn.Sequential(
+                nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(mid_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
+            # nn.BatchNorm2d(mid_channels),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            # nn.BatchNorm2d(out_channels),
         )
 
     def forward(self, x):
@@ -40,11 +48,11 @@ class Down(nn.Module):
 class DownDoubleConv(nn.Module):
     """Downscaling with maxpool then double conv"""
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, activations=True):
         super(DownDoubleConv, self).__init__()
         self.down_doubleconv = nn.Sequential(
             Down(),
-            DoubleConv(in_channels, out_channels)
+            DoubleConv(in_channels, out_channels, activations=activations)
         )
 
     def forward(self, x):
@@ -54,16 +62,16 @@ class DownDoubleConv(nn.Module):
 class UpDoubleConv(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, bilinear=True, activations=True):
         super(UpDoubleConv, self).__init__()
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.double_conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.double_conv = DoubleConv(in_channels, out_channels, in_channels // 2, activations=activations)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.double_conv = DoubleConv(in_channels, out_channels)
+            self.double_conv = DoubleConv(in_channels, out_channels, activations=activations)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -84,7 +92,6 @@ class OutConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-    
 
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=False, checkpointing=False):
@@ -94,17 +101,17 @@ class UNet(nn.Module):
         self.bilinear = bilinear
         self.checkpointing = checkpointing
 
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (DownDoubleConv(64, 128))
-        self.down2 = (DownDoubleConv(128, 256))
-        self.down3 = (DownDoubleConv(256, 512))
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = DownDoubleConv(64, 128)
+        self.down2 = DownDoubleConv(128, 256)
+        self.down3 = DownDoubleConv(256, 512)
         factor = 2 if bilinear else 1
-        self.down4 = (DownDoubleConv(512, 1024 // factor))
-        self.up1 = (UpDoubleConv(1024, 512 // factor, bilinear))
-        self.up2 = (UpDoubleConv(512, 256 // factor, bilinear))
-        self.up3 = (UpDoubleConv(256, 128 // factor, bilinear))
-        self.up4 = (UpDoubleConv(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.down4 = DownDoubleConv(512, 1024 // factor)
+        self.up1 = UpDoubleConv(1024, 512 // factor, bilinear)
+        self.up2 = UpDoubleConv(512, 256 // factor, bilinear)
+        self.up3 = UpDoubleConv(256, 128 // factor, bilinear)
+        self.up4 = UpDoubleConv(128, 64, bilinear, activations=False)
+        self.outc = OutConv(64, n_classes)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
