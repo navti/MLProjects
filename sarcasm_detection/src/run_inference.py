0e9d82c88fa87200934d48be4a0a2982e2dc94a7
Navin Tiwari <navintiwari08@yahoo.com>
2024-04-16 21:46:16 +0530

    Fix bug. Add inference for sarcasm detection.

---
+++ sarcasm_detection/src/run_inference.py
@@ -0,0 +1,19 @@
+import pickle
+from preprocess import *
+
+# load model and vectorizer
+model_dir = '../models'
+load_model_path = f"{model_dir}/model.pkl"
+with open(load_model_path, "rb") as f:
+    loaded_model = pickle.load(f)
+
+vectorizer_path = f"{model_dir}/vectorizer.pkl"
+with open(vectorizer_path, "rb") as v:
+    vectorizer = pickle.load(v)
+
+while True:
+    prompt = input('\nEnter text to check for sarcasm or ctrl+c to exit.\n')
+    clean_text = preprocess_text(prompt)
+    vec = vectorizer.transform([clean_text])
+    is_sarcasm = loaded_model.predict(vec).item()
+    print("Sarcasm!\n" if is_sarcasm else "Looks like you are serious.\n")
