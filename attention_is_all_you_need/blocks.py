3e53685b99e05f2a0f95c0f97596b82073128c0a
Navin Tiwari <navintiwari08@yahoo.com>
2024-10-02 00:24:51 +0530

    Fix encoder/decoder blocks. No weight sharing b/w modules within the blocks.

--- attention_is_all_you_need/blocks.py
+++ attention_is_all_you_need/blocks.py
@@ -7,34 +7,37 @@ class EncoderBlock(nn.Module):
     def __init__(self, d_model, num_attn_heads):
         super(EncoderBlock, self).__init__()
         self.multi_head_attn = MultiHeadSelfAttention(d_model, num_attn_heads)
-        self.layer_norm = LayerNorm(d_model)
+        self.layer_norm1 = LayerNorm(d_model)
+        self.layer_norm2 = LayerNorm(d_model)
         self.ff = FeedForward(d_model)
 
-    def forward(self, x):
-        multihead_out = self.multi_head_attn(x)
-        addnorm_out1 = self.layer_norm(x + multihead_out)
+    def forward(self, x, mask=None):
+        multihead_out = self.multi_head_attn(x, mask)
+        addnorm_out1 = self.layer_norm1(x + multihead_out)
         ff_out = self.ff(addnorm_out1)
-        addnorm_out2 = self.layer_norm(addnorm_out1 + ff_out)
+        addnorm_out2 = self.layer_norm2(addnorm_out1 + ff_out)
         return addnorm_out2
 
 class DecoderBlock(nn.Module):
     def __init__(self, d_model, num_attn_heads):
         super(DecoderBlock, self).__init__()
-        self.multi_head_attn = MultiHeadSelfAttention(d_model, num_attn_heads)
-        # self.multi_head_attn2 = MultiHeadSelfAttention(d_model, num_attn_heads)
+        self.multi_head_attn1 = MultiHeadSelfAttention(d_model, num_attn_heads)
+        self.multi_head_attn2 = MultiHeadSelfAttention(d_model, num_attn_heads)
+        self.layer_norm1 = LayerNorm(d_model)
+        self.layer_norm2 = LayerNorm(d_model)
+        self.layer_norm3 = LayerNorm(d_model)
         self.ff = FeedForward(d_model)
-        self.layer_norm = LayerNorm(d_model)
 
     def forward(self, x, enc=None, mask=None):
         # multihead attn and add and norm 1
-        multihead_out1 = self.multi_head_attn(x, mask)
-        addnorm_out1 = self.layer_norm(x + multihead_out1)
+        multihead_out1 = self.multi_head_attn1(x, mask)
+        addnorm_out1 = self.layer_norm1(x + multihead_out1)
         # multi head cross attn and addnorm layer 2
-        multihead_out2 = self.multi_head_attn(addnorm_out1, mask, True, enc)
-        addnorm_out2 = self.layer_norm(addnorm_out1 + multihead_out2)
+        multihead_out2 = self.multi_head_attn2(addnorm_out1, mask, True, enc)
+        addnorm_out2 = self.layer_norm2(addnorm_out1 + multihead_out2)
         # feed forward, add norm layer last
         ff_out = self.ff(addnorm_out2)
-        addnorm_out3 = self.layer_norm(addnorm_out2 + ff_out)
+        addnorm_out3 = self.layer_norm3(addnorm_out2 + ff_out)
         return addnorm_out3
 
 # test
