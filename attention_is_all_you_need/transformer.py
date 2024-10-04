724e55408d54fab8348bca9aa11a76f6221706d0
Navin Tiwari <navintiwari08@yahoo.com>
2024-10-04 20:13:35 +0530

    Add support for different numbers of encoder and decoder blocks in transformer.

--- attention_is_all_you_need/transformer.py
+++ attention_is_all_you_need/transformer.py
@@ -32,8 +32,16 @@ class TransformerDecoder(nn.Module):
 class Transformer(nn.Module):
     def __init__(self, *args, **kwargs):
         super(Transformer, self).__init__()
-        self.encoder = TransformerEncoder(*args, **kwargs)
-        self.decoder = TransformerDecoder(*args, **kwargs)
+        self.num_enc = kwargs.get('num_enc') if 'num_enc' in kwargs else args[0]
+        self.num_dec = kwargs.get('num_dec') if 'num_dec' in kwargs else args[1]
+        self.d_model = kwargs.get('d_model') if 'd_model' in kwargs else args[2]
+        self.num_attn_heads = kwargs.get('num_attn_heads') if 'num_attn_heads' in kwargs else args[3]
+        self.vocab_size = kwargs.get('vocab_size') if 'vocab_size' in kwargs else args[4]
+        self.max_seq_len = kwargs.get('max_seq_len') if 'max_seq_len' in kwargs else args[5]
+        self.pad_idx = kwargs.get('pad_idx') if 'pad_idx' in kwargs else args[6] if len(args) == 7 else None
+
+        self.encoder = TransformerEncoder(self.num_enc, self.d_model, self.num_attn_heads, self.vocab_size, self.max_seq_len, self.pad_idx)
+        self.decoder = TransformerDecoder(self.num_dec, self.d_model, self.num_attn_heads, self.vocab_size, self.max_seq_len, self.pad_idx)
 
     def forward(self, enc_token_ids, dec_token_ids, enc_mask=None, dec_mask=None):
         enc_out = self.encoder(enc_token_ids, enc_mask)
@@ -43,14 +51,14 @@ class Transformer(nn.Module):
 # test
 if __name__ == "__main__":
     num_enc = 3
-    num_dec = 3
+    num_dec = 5
     d_model = 128
     num_attn_heads = 8
     batch_size = 2
     seq_len = 5
     token_ids = torch.randint(0, 10, size=(batch_size, seq_len))
     mask = torch.tensor([1,1,1,0,0])
-    tr = Transformer(num_enc, d_model, num_attn_heads, vocab_size=20, max_seq_len=10)
+    tr = Transformer(num_enc, num_dec, d_model, num_attn_heads, vocab_size=20, max_seq_len=10)
     out = tr(token_ids, token_ids, None, mask)
     summary(tr)
     print(out.shape)
