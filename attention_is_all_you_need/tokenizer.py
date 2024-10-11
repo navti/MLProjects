23d617a774e67e0a4837b4ab2986c7057a444193
Navin Tiwari <navintiwari08@yahoo.com>
2024-10-11 23:14:56 +0530

    Add encode, decode methods to tokenizer class.

--- attention_is_all_you_need/tokenizer.py
+++ attention_is_all_you_need/tokenizer.py
@@ -3,13 +3,80 @@ from collections import defaultdict
 from tqdm import tqdm
 from copy import deepcopy
 
+class BPETokenzer:
+    def __init__(self):
+        self._merges = {}
+        self._decode_vocab = {}
+        self._vocab = {}
+
+    def get_stats(self, token_ids):
+        pair_counts = defaultdict(int)
+        for pair in zip(token_ids, token_ids[1:]):
+            pair_counts[pair] += 1
+        return pair_counts
+
+    def encode(self, document):
+        tokens = list(document.encode('utf-8'))
+        while True:
+            pair_counts = self.get_stats(tokens)
+            pair = min(pair_counts, key=lambda k: self._merges.get(k, float('inf')))
+            if pair in self._merges:
+                tokens = self.merge(tokens, pair, self._merges[pair])
+            else:
+                break
+        return tokens
+
+    def decode(self, ids):
+        tokens = b''.join(self._vocab[idx] for idx in ids)
+        return tokens.decode('utf-8', errors='replace')
+
+    def merge(self, tokens, pair, token_id):
+        new_tokens = []
+        i = 0
+        while i < len(tokens):
+            if i < len(tokens)-1 and (tokens[i], tokens[i+1]) == pair:
+                new_tokens.append(token_id)
+                i += 2
+            else:
+                new_tokens.append(tokens[i])
+                i += 1
+        return new_tokens
+
+    def train(self, train_corpus, num_merges):
+        corpus = deepcopy(train_corpus)
+        last_token_id = 255
+        for _ in tqdm(range(num_merges)):
+            pair_count = defaultdict(int)
+            word_idx = defaultdict(set)
+            for idx, word in enumerate(corpus):
+                for i in range(1, len(word)):
+                    pair = (word[i-1], word[i])
+                    pair_count[pair] += 1
+                    word_idx[pair].add(idx)
+            if pair_count:
+                top_pair = max(pair_count, key=pair_count.get)
+                merged_token_id = last_token_id + 1
+                last_token_id = merged_token_id
+                self._merges[top_pair] = merged_token_id
+                for idx in range(len(corpus)):
+                    if idx in word_idx[top_pair]:
+                        word = corpus[idx]
+                        corpus[idx] = self.merge(word, top_pair, merged_token_id)
+            else:
+                break
+        vocab = {key: bytes([key]) for key in range(256)}
+        for (a,b), idx in self._merges.items():
+            vocab[idx] = vocab[a] + vocab[b]
+        self._vocab = vocab
+
 def build_corpus(sentences):
     corpus = []
     for sentence in sentences:
         words = sentence.split()
         for word in words:
             word = word.lower() + ' '
-            word = list(word)
+            word = word.encode('utf-8')
+            word = list(map(int, word))
             corpus.append(word)
     return corpus
 
@@ -20,6 +87,16 @@ def df_to_corpus(df):
     h_cor = build_corpus(hindi_sentences)
     return e_cor, h_cor
 
+df = pd.read_csv('data/train.csv')
+eng_corpus, hindi_corpus = df_to_corpus(df)
+en_tokenizer = BPETokenzer()
+en_tokenizer.train(eng_corpus, 10)
+print('done')
+
+
+
+"""
+# for character based pairing and encoding, not used in BPE.
 def save_base_vocab(name, corpus):
     base_vocab = set()
     for word in corpus:
@@ -35,44 +112,18 @@ def load_base_vocab(name):
             base_vocab.add(ch)
     return base_vocab
 
-def build_vocab(corpus, base_vocab, num_merges):
-    merges = {}
-    vocab = deepcopy(base_vocab)
-    for _ in tqdm(range(num_merges)):
-        pair_count = defaultdict(int)
-        word_idx = defaultdict(set)
-        for idx, word in enumerate(corpus):
-            for i in range(1, len(word)):
-                pair = (word[i-1], word[i])
-                pair_count[pair] += 1
-                word_idx[pair].add(idx)
-        if pair_count:
-            top_pair = max(pair_count, key=pair_count.get)
-            merged_token = ''.join(top_pair)
-            merges[top_pair] = merged_token
-            vocab.add(merged_token)
-            for idx in range(len(corpus)):
-                if idx in word_idx[top_pair]:    
-                    word = corpus[idx]
-                    new_word = []
-                    i = 0
-                    while i < len(word):
-                        if i < len(word)-1 and (word[i], word[i+1]) == top_pair:
-                            new_word.append(merged_token)
-                            i += 2
-                        else:
-                            new_word.append(word[i])
-                            i += 1
-                    corpus[idx] = new_word
-        else:
-            break
-    return merges, vocab
+def save_merges(name, merges):
+    with open(name, 'w') as f:
+        for item in merges.items():
+            f.write(str(item)+'\n')
 
-df = pd.read_csv('data/train.csv')
-eng_corpus, hindi_corpus = df_to_corpus(df)
-# save_base_vocab('base_vocab_en.txt', eng_corpus)
-# save_base_vocab('base_vocab_hi.txt', hindi_corpus)
-# merges, vocab = build_vocab(eng_corpus, 10000)
-base_vocab_en = load_base_vocab('base_vocab_en.txt')
-merges, vocab = build_vocab(eng_corpus, base_vocab_en, 10)
-print('done')
+def load_merges(name):
+    merges = {}
+    with open('merges.txt', 'r') as f:
+        for line in f.readlines():
+            item = eval(line)
+            key = item[0]
+            val = item[1]
+            merges[key] = val
+    return merges
+"""
