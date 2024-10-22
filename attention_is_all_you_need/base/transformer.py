Use grad scaler with amp. Fix attention mask bug. Fix bug in BLEU score calculation.

attention_is_all_you_need/base/transformer.py
@@ -65,7 +65,7 @@ class TransformerDecoder(nn.Module):
         token_embeddings = self.input_embedding(token_ids)
         out = self.dec_blocks[0](token_embeddings, enc_out, mask)
         for dec_block in self.dec_blocks[1:]:
-            out = dec_block(out, enc_out)
+            out = dec_block(out, enc_out, mask)
         return out
 
 class Transformer(nn.Module):

attention_is_all_you_need/main.py
@@ -6,21 +6,23 @@ from data.dataset import TranslateSet
 from torch.utils.data import DataLoader
 from torch import nn
 from torch import optim
+from torchinfo import summary
 import sys
+from torch.optim.lr_scheduler import StepLR
 
 def get_args():
     parser = argparse.ArgumentParser(description='English to Hindi translator')
-    parser.add_argument('--dmodel', '-d', metavar='MODEL_DIM', type=int, default=128, help='Model dimension')
+    parser.add_argument('--dmodel', '-d', metavar='MODEL_DIM', type=int, default=512, help='Model dimension')
     parser.add_argument('--attn-heads', '-a', metavar='NUM_HEADS', type=int, default=8, help='No. of self attention heads')
     parser.add_argument('--nenc', metavar='NUM_ENC', type=int, default=6, help='No. of encoder blocks in the transformer')
     parser.add_argument('--ndec', metavar='NUM_DEC', type=int, default=6, help='No. of decoder blocks in the transformer')
     parser.add_argument('--context-length', '-c', metavar='LENGTH', type=int, default=32, help='Max no. of tokens in the input sequence')
-    parser.add_argument('--epochs', '-e', metavar='NUM_EPOCHS', type=int, default=5, help='Number of epochs')
+    parser.add_argument('--epochs', '-e', metavar='NUM_EPOCHS', type=int, default=20, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='BATCH_SIZE', type=int, default=64, help='Batch size')
-    parser.add_argument('--lr', '-l', metavar='LR', type=float, default=1e-5, help='Learning rate', dest='lr')
+    parser.add_argument('--lr', '-l', metavar='LR', type=float, default=5e-5, help='Learning rate', dest='lr')
     parser.add_argument('--train', '-t', action='store_true', default=False, help='Train model with given parameters.')
     parser.add_argument('--save-model', action='store_true', default=False, help='For Saving the current Model')
-    parser.add_argument('--load', '-f', metavar='MODEL_PATH', type=str, default="", help='Load model from a .pth file. Required when running inference.')
+    parser.add_argument('--load', '-f', metavar='MODEL_PATH', type=str, default=None, help='Load model from a .pth file. Required when running inference.')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
     return parser, parser.parse_args()
 
@@ -49,7 +51,9 @@ if __name__ == "__main__":
     model_kwargs['enc_pad_idx'] = en_tokenizer.special_tokens["[PAD]"]
     model_kwargs['dec_pad_idx'] = hi_tokenizer.special_tokens["[PAD]"]
     model_kwargs['device'] = device
-
+    # models_dir = f"{proj_dir}/model/saved_models"
+    # args.load = f"{models_dir}/translateEnHibase.pth"
+    # args.train = True
     if args.train:
         train_kwargs = {'batch_size': args.batch_size}
         test_kwargs = {'batch_size': args.batch_size}
@@ -71,21 +75,32 @@ if __name__ == "__main__":
         test_loader = DataLoader(test_set, **test_kwargs)
         validation_loader = DataLoader(validation_set, **validation_kwargs)
 
-        model = Translator(**model_kwargs).to(device)
+        if args.load != None:
+            print(f"Training existing model.")
+            print(f"Loading model from the given path: {args.load}")
+            model = load_model(args.load, **model_kwargs).to(device)
+        else:
+            model = Translator(**model_kwargs).to(device)
         loss_criterion = nn.CrossEntropyLoss()
         optimizer = optim.AdamW(model.parameters(), weight_decay=1e-2, lr=args.lr, betas=(0.9, 0.999))
+        scheduler = StepLR(optimizer, step_size=100, gamma=0.5)
+        grad_scaler = torch.amp.GradScaler(enabled=True)
+        print("Model summary:")
+        summary(model)
 
         # training loop
+        print("\nTraining model:")
         losses = {'train':[], 'validation':[]}
         scores = {'bleu':[]}
         for epoch in range(1, args.epochs+1):
             # train
-            avg_epoch_loss = train(model, device, epoch, optimizer, train_loader, loss_criterion)
+            avg_epoch_loss = train(model, device, epoch, optimizer, train_loader, loss_criterion, grad_scaler)
             losses['train'].append(avg_epoch_loss)
             # evaluate
             avg_loss, avg_bleu_score = evaluate(model, device, epoch, validation_loader, loss_criterion, hi_tokenizer)
             losses['validation'].append(avg_loss)
             scores['bleu'].append(avg_bleu_score)
+            scheduler.step()
 
         results_dir = f"{proj_dir}/results"
         models_dir = f"{proj_dir}/model/saved_models"
@@ -96,7 +111,6 @@ if __name__ == "__main__":
         if (args.save_model):
             save_model(model, models_dir, name=model_name)
     else:
-        models_dir = f"{proj_dir}/model/saved_models"
         model = load_model(args.load, **model_kwargs)
         if model == None:
             print("Check if the correct model path was provided to load from.")

attention_is_all_you_need/model/model.py
@@ -31,6 +31,7 @@ def adjust_predictions(predictions, p_mask, pad_id):
     return predictions
 
 def infer(sent, context_len, model, device, en_tokenizer, hi_tokenizer):
+    model.eval()
     enc_in, _ = process_sentence(sent, context_len, en_tokenizer)
     enc_in = enc_in.unsqueeze(dim=0).to(device)
     out_tokens = []
@@ -47,7 +48,7 @@ def infer(sent, context_len, model, device, en_tokenizer, hi_tokenizer):
         next_token = predictions[len(out_tokens)-1].item()
     return hi_tokenizer.decode_nice(out_tokens)
 
-def train(model, device, epoch, optimizer, train_loader, loss_criterion):
+def train(model, device, epoch, optimizer, train_loader, loss_criterion, grad_scaler):
     model.train()
     dec_pad_idx = model.transformer.dec_pad_idx
     total_loss = 0
@@ -57,14 +58,17 @@ def train(model, device, epoch, optimizer, train_loader, loss_criterion):
         dec_in = dec_in.to(device)
         pad_mask = pad_mask[:,1:].to(device)
         targets = dec_in[:,1:]
+        with torch.autocast(device.type, enabled=True):
+            predictions = model(enc_in, dec_in[:,:-1])
+            predictions = adjust_predictions(predictions, pad_mask, dec_pad_idx)
+            predictions = torch.einsum('pqr->prq', predictions)
+            loss = loss_criterion(predictions, targets)
+            total_loss += (loss.item() / batch_size)
         optimizer.zero_grad()
-        predictions = model(enc_in, dec_in[:,:-1])
-        predictions = adjust_predictions(predictions, pad_mask, dec_pad_idx)
-        predictions = torch.einsum('pqr->prq', predictions)
-        loss = loss_criterion(predictions, targets)
-        total_loss += (loss.item() / batch_size)
-        loss.backward()
-        optimizer.step()
+        grad_scaler.scale(loss).backward()
+        grad_scaler.unscale_(optimizer)
+        grad_scaler.step(optimizer)
+        grad_scaler.update()
         if batch_idx % 100 == 0:
             print(f"Avg loss after batch {batch_idx+1}/{len(train_loader)}: {(total_loss/(batch_idx+1)):.4f}")
     avg_epoch_loss = total_loss/(batch_idx+1)
@@ -84,14 +88,14 @@ def evaluate(model, device, epoch, validation_loader, loss_criterion, tokenizer)
         targets = dec_in[:,1:]
         predictions = model(enc_in, dec_in[:,:-1])
         predictions = adjust_predictions(predictions, pad_mask, dec_pad_idx)
-        out_tokens = predictions.argmax(dim=-1)
+        out_tokens = predictions.argmax(dim=-1).squeeze()
         predictions = torch.einsum('pqr->prq', predictions)
         loss = loss_criterion(predictions, targets)
         total_loss += (loss.item() / batch_size)
         total_bleu_score += get_batch_bleu_score(out_tokens, targets, tokenizer)
     avg_validation_loss = total_loss/(batch_idx+1)
     avg_bleu_score = total_bleu_score/(batch_idx+1)
-    print(f"Validation after Epoch {epoch}:\n\tLoss: {avg_validation_loss:.2f}\n\tAvg BLEU score: {avg_bleu_score:.2f}")
+    print(f"Validation after Epoch {epoch}:\n\tLoss: {avg_validation_loss:.4f}\n\tAvg BLEU score: {avg_bleu_score:.4f}")
     return avg_validation_loss, avg_bleu_score
 
 def save_model(model, models_dir, name=None):
@@ -111,7 +115,6 @@ def save_model(model, models_dir, name=None):
 # load model
 def load_model(model_path, *args, **kwargs):
     model = Translator(*args, **kwargs)
-    # model_path = "results/saved_models/"+model_name+".pth"
     try:
         model.load_state_dict(torch.load(model_path, weights_only=True))
     except OSError as e: