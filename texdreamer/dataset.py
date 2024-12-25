ced6028a509b0ce461992254ee737454808aaa15
Navin Tiwari <navintiwari08@yahoo.com>
2024-12-26 00:48:03 +0530

    HF Atlas dataset doesn't have prompt. Use separate txt file to load prompts.

--- texdreamer/dataset.py
+++ texdreamer/dataset.py
@@ -6,17 +6,25 @@ from torch.utils.data import Dataset
 
 
 class HuggingFaceATLAS(Dataset):
-    def __init__(self, split="train"):
+    def __init__(self, split="train", prompts_file="atlas_prompt_50k.txt"):
         self.dataset = load_dataset("ggxxii/ATLAS", split=split)
+        self.prompts = self._load_prompts(prompts_file)
+        assert len(self.dataset) == len(
+            self.prompts
+        ), "Mismatch between number of images and prompts."
+
         self.transform = transforms.Compose(
             [transforms.Resize((512, 512)), transforms.ToTensor()]
         )
+
+    def _load_prompts(self, prompts_file):
+        with open(prompts_file, "r", encoding="utf-8") as f:
+            return [line.strip() for line in f.readlines() if line.strip()]
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        item = self.dataset[idx]
-        prompt = item["prompt"]
-        image = self.transform(item["image"]).float()
+        image = self.transform(self.dataset[idx]["image"]).float()
+        prompt = self.prompts[idx]
         return {"prompt": prompt, "uv_image": image}
