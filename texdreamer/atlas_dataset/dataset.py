ce9de8ac5203685a90374db9431763997af5d809
Navin Tiwari <navintiwari08@yahoo.com>
2025-01-13 14:12:42 +0530

    Update custom dataset to use atlas large. Rename build dataset script.

--- texdreamer/atlas_dataset/dataset.py
+++ texdreamer/atlas_dataset/dataset.py
@@ -1,7 +1,7 @@
 # dataset.py
 import os
 from PIL import Image
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from torchvision import transforms
 from torch.utils.data import Dataset
 
@@ -38,26 +38,44 @@ class HuggingFaceATLAS(Dataset):
         return {"prompt": prompt, "uv_image": image}
 
 
-class AtlasI2UVDataset(Dataset):
-    def __init__(self, split="train", prompts_file=None, cache_dir=None):
-        self.dataset = load_dataset("ggxxii/ATLAS", split=split, cache_dir=cache_dir)
-        self.transform = transforms.Compose(
+class ATLASLarge(Dataset):
+    def __init__(
+        self,
+        task="t2uv",
+        cache_dir="/mnt/ssdp3/datasets/atlas_large",
+        resize_tex=1024,
+        resize_img=(1024, 768),
+        normalize=True,
+    ):
+        self.task = task
+        self.dataset = load_from_disk(cache_dir)
+        tex_transforms = transforms.Compose(
             [
-                transforms.Resize((512, 512)),
-                transforms.ToTensor(),  # Converts to [0,1]
+                transforms.Resize((resize_tex, resize_tex)),
+                transforms.Lambda(lambda img: img.convert("RGB")),
+                transforms.ToTensor(),
             ]
         )
+        if normalize:
+            tex_transforms.append(transforms.Normalize(mean=[0.5] * 4, std=[0.5] * 4))
+        self.transform_tex = transforms.Compose(tex_transforms)
+
+        if task == "i2uv":
+            img_transforms = [transforms.Resize(resize_img), transforms.ToTensor()]
+            if normalize:
+                img_transforms.append(
+                    transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)
+                )
+            self.transform_img = transforms.Compose(img_transforms)
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        image = self.dataset[idx]["image"].convert("RGB")
-        uv_image = self.dataset[idx]["uv_texture"]  # This may vary based on key name
-        if isinstance(uv_image, Image.Image):
-            uv_image = uv_image.convert("RGB")
-
-        image_tensor = self.transform(image)
-        uv_tensor = self.transform(uv_image)
-
-        return {"image": image_tensor, "uv_image": uv_tensor}
+        texture = self.transform_tex(self.dataset[idx]["texture"])
+        prompt = self.dataset[idx]["prompt"]
+        items = {"prompt": prompt, "texture": texture}
+        if self.task == "i2uv":
+            image = self.transform_img(self.dataset[idx]["image"])
+            items["image"] = image
+        return items
